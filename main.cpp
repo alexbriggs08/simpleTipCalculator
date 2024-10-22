1640ceaddefdbf7a4922325857749ea4a5efa4c6
alex <alexbr1208@gmail.com>
2024-10-22 13:41:20 -0400

    -- fixed it yay!

--- main.cpp
+++ main.cpp
@@ -15,32 +15,26 @@ int main() {
     double total {}; // sum of the tip and price
 
 
-    while (!conformationQuit) {
-
-        switch (tipPercentage) {
-            case 15:
-                tipAmmount = (price * .15);
-            break;
-            case 18:
-                tipAmmount = (price * .18);
-            break;
-            case 20:
-                tipAmmount = price * .20;
-            break;
-            case 25:
-                tipAmmount = price * .25;
-            break;
-            default:
-                std::cout << "Something went wrong: Please enter a valid tip percentage." << std::endl;
-            return 1; //exit with an error code
-
-        }
-
-        total = tipAmmount + price;
-        std::cout << "The total including tip is \n" << total << '\n';
-        std::cout << "The tip ammount is: \n" << tipAmmount << std::endl;
-
-
+    switch (tipPercentage) {
+        case 15:
+            tipAmmount = (price * .15);
+        break;
+        case 18:
+            tipAmmount = (price * .18);
+        break;
+        case 20:
+            tipAmmount = price * .20;
+        break;
+        case 25:
+            tipAmmount = price * .25;
+        break;
+        default:
+            std::cout << "Something went wrong: Please enter a valid tip percentage." << std::endl;
+        return 1; //exit with an error code
 
     }
+
+    total = tipAmmount + price;
+    std::cout << "The total including tip is \n" << total << '\n';
+    std::cout << "The tip ammount is: \n" << tipAmmount << std::endl;
 }
